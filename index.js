2da43b3e0e5552b719e15e0353c8415d31606515
SehwarMaryam <shehwarm93@gmail.com>
2025-05-25 14:39:30 +0500

    Add image slider functionality with HTML, CSS, and JavaScript

---
+++ index.js
@@ -0,0 +1,20 @@
+// IMAGE SLIDER
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalid = null;
+
+function initializeSlider(){
+
+    slides[slideIndex].classList.add("displaySlide");
+
+}
+function showSlide(index) {
+
+}
+function prevSlide(){
+
+}
+function nextSlide() {
+
+}
